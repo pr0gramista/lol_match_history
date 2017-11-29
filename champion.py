8696d95e0fdf5ebad03c39ddebb7d5924d1f4bc7
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-11-29 21:37:38 +0100

    Add function to get champion name from id

--- champion.py
+++ champion.py
@@ -1,13 +1,24 @@
 import json
 
-champion_id = input("Champion id:\n")
-
 with open('champion.json', 'r', encoding='utf-8') as f:
     champion_data = f.read()
 
 champion_json = json.loads(champion_data)
 
+__champions = {}
+
 for champion_name in champion_json['data'].keys():
-    if champion_json['data'][champion_name]['key'] == champion_id:
-        print("It's {}".format(champion_name))
-        break
+    __champions.update({
+        int(champion_json['data'][champion_name]['key']): champion_name
+    })
+
+
+def get_champion_name_for_id(i):
+    if i in __champions:
+        return __champions[i]
+    else:
+        raise KeyError()
+
+
+if __name__ == '__main__':
+    print(get_champion_name_for_id(int(input("Champion id:\n"))))
